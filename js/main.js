9d1d041fa311cd44dbb5eb56ab81735e9e3dadfa
PaulSapronov <pavel.sapronov@mail.ru>
2020-11-25 23:23:09 +0700

    create: added ymaps and placemark in map

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ var mySwiper = new Swiper('.swiper-container', {
 	keyboard: true,
 	effect: 'slide',
 });
+
+ymaps.ready(init);
+
+function init() {
+	var map = new ymaps.Map('map', {
+		center: [7.83817708, 98.29881664],
+		zoom: 17
+		/* type: 'yandex#satellite' */
+	});
+	var placemark = new ymaps.Placemark([7.83817708, 98.29881664], {
+		balloonContent: 'Два часа потратил на долбаную метку!:)'
+	}, {
+		preset: 'islands#icon',
+		iconColor: '#0095b6'
+	});
+
+	map.geoObjects.add(placemark);
+}
