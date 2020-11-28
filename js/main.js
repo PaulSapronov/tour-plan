4801e3b09a69895c9047f1b2f69883fb538a5eda
PaulSapronov <pavel.sapronov@mail.ru>
2020-11-28 19:36:18 +0700

    fixed html

--- js/main.js
+++ js/main.js
@@ -22,22 +22,3 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 	},
 	keyboard: true,
 });
-
-//! Edd Yandex Maps
-ymaps.ready(init);
-
-function init() {
-	var map = new ymaps.Map('map', {
-		center: [7.83817708, 98.29881664],
-		zoom: 17
-		/* type: 'yandex#satellite' */
-	});
-	var placemark = new ymaps.Placemark([7.83817708, 98.29881664], {
-		balloonContent: 'Два часа потратил на долбаную метку!:)'
-	}, {
-		preset: 'islands#icon',
-		iconColor: '#0095b6'
-	});
-
-	map.geoObjects.add(placemark);
-}
