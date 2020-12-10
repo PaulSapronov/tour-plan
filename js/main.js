d9233a365c924459352e29d20788f71b04d305d7
PaulSapronov <pavel.sapronov@mail.ru>
2020-12-10 20:48:37 +0700

    add form validation

--- js/main.js
+++ js/main.js
@@ -57,7 +57,26 @@ $(document).ready(function () {
 			modalDialog.removeClass('modal__dialog--visible');
 		}
 	});
-	// Обработка форм
+
+	$('.form').each(function () {
+		$(this).validate({
+			errorClass: 'invalid',
+			messages: {
+				name: {
+					required: 'Please specify your name',
+				},
+				email: {
+					required: 'We need your email address to contact you',
+					email: 'Your email address must be in the format of name@domain.com',
+				},
+				phone: {
+					required: 'Please specify your phone number',
+				},
+			},
+		});
+	});
+
+
 
 	AOS.init();
 });
